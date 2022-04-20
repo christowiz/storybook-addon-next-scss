ab7b3001e6c2c7445643a2f5944029f198c29495
Christopher Gwizdala <597282+christowiz@users.noreply.github.com>
2022-04-20 12:51:36 -0700

    Add styled-jsx SCSS feature to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,25 @@
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-next'
+    // "storybook-addon-next",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
   ],
-  framework: '@storybook/react',
   core: {
-    builder: 'webpack5'
-  }
-}
+    builder: "webpack5",
+  },
+  framework: "@storybook/react",
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ["style-loader", "css-loader", "sass-loader"],
+  //     include: require("path").resolve(__dirname, "../"),
+  //   });
+
+  //   return config;
+  // },
+};
